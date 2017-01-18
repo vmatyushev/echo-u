9ebb5c1ea8033d27a77fefb49f3083d365e7c7e8
IOTS <vmatyushev@gmail.com>
2017-01-18 17:25:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,6 @@ bot = telebot.TeleBot(config.token)
   
 @bot.message_handler(func=lambda message: "молоко" in message.text)
 def moloko(message):
-     bot.send_message(message.chat.id, message.text)
+  bot.send_message(message.chat.id, message.text)
 if __name__ == '__main__':
-bot.polling(none_stop=True)
+  bot.polling(none_stop=True)
