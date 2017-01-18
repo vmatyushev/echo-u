399b08025e1fc6e240153341ed2c4d92834fe61f
IOTS <vmatyushev@gmail.com>
2017-01-18 17:48:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,9 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
   
-@bot.message_handler(func=lambda message: "молоко" in message.text)
-def moloko(message):
-  bot.send_message(message.chat.id, message.text)
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
 if __name__ == '__main__':
-  bot.polling(none_stop=True)
+     bot.polling(none_stop=True)
