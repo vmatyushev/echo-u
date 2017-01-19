415c2b02396d282df3954ab4d4319952928d7d2c
IOTS <vmatyushev@gmail.com>
2017-01-19 18:21:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,24 @@ import subprocess
 
 from subprocess import Popen, PIPE
 
-bot = telebot.TeleBot(config.token)
-  
+mi_bot = telebot.TeleBot(config.token)
+
+
+def listener(*mensajes):
+    for m in mensajes:
+        chat_id = m.chat.id
+        if m.content_type == 'text':
+            text = m.text
+            mi_bot.send_message(chat_id,"Me copio de tu texto")
+            mi_bot.send_message(chat_id, text)
+
+mi_bot.set_update_listener(listener) #registrar la funcion listener  
+mi_bot.polling()
+
+while True: #No terminamos nuestro programa  
+    pass
+
+
 #@bot.message_handler(content_types=["text"])
 #def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
 #    bot.send_message(message.chat.id, message.text)
@@ -35,12 +51,12 @@ p = subprocess.Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE,
         stderr=subprocess.STDOUT, close_fds=True, cwd='/home/')
 print(p.stdout.read())
 
-@bot.message_handler(content_types=["text"])
+#@bot.message_handler(content_types=["text"])
 
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, p)
+#def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+#    bot.send_message(message.chat.id, p)
 
-if __name__ == '__main__':
-     bot.polling(none_stop=True)
+#if __name__ == '__main__':
+#     bot.polling(none_stop=True)
 
     
