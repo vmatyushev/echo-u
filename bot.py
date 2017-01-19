a71420c10eac978f2b0a079a93f4371544d75f3c
IOTS <vmatyushev@gmail.com>
2017-01-19 14:28:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,15 @@
 import config
 import telebot
 
+import subprocess
+
 bot = telebot.TeleBot(config.token)
   
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
+    
+subprocess.call('uname -a', shell=True)
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
