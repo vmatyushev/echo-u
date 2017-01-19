a4d178e49f8531216fa69c64dc5b4b76c0ca4d2c
IOTS <vmatyushev@gmail.com>
2017-01-19 18:06:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ print(p.stdout.read())
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, p)
 
-#if __name__ == '__main__':
- #    bot.polling(none_stop=True)
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
 
     
